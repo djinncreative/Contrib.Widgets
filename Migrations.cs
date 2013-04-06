b111b650999992e2593146c0c1091b69ec8eb6a6
Sipke Schoorstra <skywalker@darkskydevelopment.net>
2013-04-06 20:11:58 +0200

    WidgetsPage + Bug fix
    Added a new WidgetsPage content type that leverages the
    WidgetsContainerPart and fixed a bug where the theme does not have a
    preview image.

--- Migrations.cs
+++ Migrations.cs
@@ -18,7 +18,39 @@ namespace Contrib.Widgets {
                 .Attachable()
                 .WithSetting("ContentPartSettings.Description", "Enables content items to contain widgets, removing the need to create a layer rule per content item."));
 
-            return 1;
+            ContentDefinitionManager.AlterTypeDefinition("WidgetsPage", type => type
+                .WithPart("CommonPart", p => p
+                    .WithSetting("DateEditorSettings.ShowDateEditor", "true"))
+                .WithPart("PublishLaterPart")
+                .WithPart("TitlePart")
+                .WithPart("AutoroutePart", part => part
+                    .WithSetting("AutorouteSettings.AllowCustomPattern", "true")
+                    .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
+                    .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Title', Pattern: '{Content.Slug}', Description: 'my-page'}]")
+                    .WithSetting("AutorouteSettings.DefaultPatternIndex", "0"))
+                .WithPart("BodyPart")
+                .WithPart("WidgetsContainerPart")
+                .Creatable());
+
+            return 2;
+        }
+
+        public int UpdateFrom1() {
+            ContentDefinitionManager.AlterTypeDefinition("WidgetsPage", type => type
+                .WithPart("CommonPart", p => p
+                    .WithSetting("DateEditorSettings.ShowDateEditor", "true"))
+                .WithPart("PublishLaterPart")
+                .WithPart("TitlePart")
+                .WithPart("AutoroutePart", part => part
+                    .WithSetting("AutorouteSettings.AllowCustomPattern", "true")
+                    .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
+                    .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Title', Pattern: '{Content.Slug}', Description: 'my-page'}]")
+                    .WithSetting("AutorouteSettings.DefaultPatternIndex", "0"))
+                .WithPart("BodyPart")
+                .WithPart("WidgetsContainerPart")
+                .Creatable());
+
+            return 2;
         }
     }
 }
